13730e9d6ae55027c7e6badd2f3daa1628dc882f
Aaron Evans <aaron.evans@saucelabs.com>
2017-10-04 07:35:21 -0600

    add testobject url and desired capabilities to skeleton test

--- src/test/java/SimpleIOSCalculatorTest.java
+++ src/test/java/SimpleIOSCalculatorTest.java
@@ -1,7 +1,9 @@
 import io.appium.java_client.ios.IOSDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -15,15 +17,33 @@ public class SimpleIOSCalculatorTest
 	DesiredCapabilities desiredCapabilities;
 	URL webdriverURL;
 	
+	@Rule
+	public TestName testName = new TestName();
+	
 	@Before
 	public void setup() throws MalformedURLException
 	{
+		webdriverURL = new URL("https://us1.appium.testobject.com/wd/hub");
+		
+		desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setCapability("testobject_api_key", System.getenv("TESTOBJECT_API_KEY"));
+		desiredCapabilities.setCapability("testobject_suite_name", this.getClass().getSimpleName());
+		desiredCapabilities.setCapability("testobject_test_name", testName.getMethodName());
+		desiredCapabilities.setCapability("appiumVersion", "1.6.5");
+		desiredCapabilities.setCapability("platformName", "iOS");
+		desiredCapabilities.setCapability("platformVersion", "10.3");
+		desiredCapabilities.setCapability("phoneOnly", "yes");
+		
+		driver = new IOSDriver(webdriverURL, desiredCapabilities);
 	}
 	
 	@After
 	public void cleanup()
 	{
-	
+		if (driver != null)
+		{
+			driver.quit();
+		}
 	}
 	
 	@Test
